725edb349ded7e0b96181875e6e20533e3dd854d
Natasha Selvidge <Natasha.selvidge@gmail.com>
2020-12-23 14:28:25 -0800

    commit tree 4220df45d603d1c3278e2e12036c3eb845ced91a

--- app.rb
+++ app.rb
@@ -1,5 +1,19 @@
 require_relative 'config/environment'
-
+#user goes to home page
+#follow link to a form where user input puppy info
+#when user submits view puppy info
 class App < Sinatra::Base
+  get '/' do 
+    erb :index
+  end
+
+  get '/new' do
+    erb :create_puppy
+  end
+
+  post '/puppy' do 
+    @puppy = Puppy.new(params[:name],params[:breed],params[:age])
+    erb :display_puppy
+  end 
 
 end
